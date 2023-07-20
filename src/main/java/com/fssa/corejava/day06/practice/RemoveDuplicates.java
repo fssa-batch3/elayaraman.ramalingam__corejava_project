0673d88b6fe5af9afd17b3e9b47a28a2bc580d54
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-07-20 07:31:27 +0530

    day 07

--- src/main/java/com/fssa/corejava/day06/practice/RemoveDuplicates.java
+++ src/main/java/com/fssa/corejava/day06/practice/RemoveDuplicates.java
@@ -1,24 +1,69 @@
 package com.fssa.corejava.day06.practice;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RemoveDuplicates {
-	
-	private static void main(String[] args) {
-		List<String> cityList = new ArrayList<String>();
-		cityList.add("Chennai");
-		cityList.add("Bangalore");
-		cityList.add("Mumbai");
-		cityList.add("Mumbai");
-		List<String> newList = new ArrayList<>();
-		for(String i : cityList) {
-			if(!newList.contains(i)) {
-				newList.add(i);
-			}
-		}
-		System.out.println(newList);
-		
-	}
 
+    public static void main(String[] args) {
+        List<Tasku> cityList = new ArrayList<>();
+        cityList.add(new Tasku(1, "One", LocalDate.of(2023, 8, 7)));
+        cityList.add(new Tasku(2, "Two", LocalDate.of(2023, 8, 7)));
+        cityList.add(new Tasku(3, "Three", LocalDate.of(2023, 8, 7)));
+        cityList.add(new Tasku(3, "Three", LocalDate.of(2023, 8, 7)));
+        cityList.add(new Tasku(4, "Four", LocalDate.of(2023, 8, 7)));
+        cityList.add(new Tasku(4, "Four", LocalDate.of(2023, 8, 7)));
+
+        List<Tasku> newList = new ArrayList<>();
+        Set<String> names = new HashSet<>();
+
+        for (Tasku i : cityList) {
+            if (!names.contains(i.getName())) {
+                newList.add(i);
+                names.add(i.getName());
+            }
+        }
+
+        System.out.println(newList);
+    }
+
+}
+
+class Tasku {
+    private int id;
+    private String name;
+    private LocalDate deadline;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public Tasku(int id, String name, LocalDate deadline) {
+        this.id = id;
+        this.name = name;
+        this.deadline = deadline;
+    }
 }
